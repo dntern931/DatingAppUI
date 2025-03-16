e7ba6691e3c7f1a3174d963656f5e95ac38ffafc
Bryan Ramirez <dntern931@yahoo.com>
2025-03-16 09:51:48 -0500

    End of Section 9

--- src/app/errors/test-errors/test-errors.component.ts
+++ src/app/errors/test-errors/test-errors.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { errorContext } from 'rxjs/internal/util/errorContext';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-test-errors',
@@ -10,7 +11,7 @@ import { errorContext } from 'rxjs/internal/util/errorContext';
   styleUrl: './test-errors.component.scss'
 })
 export class TestErrorsComponent {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   validationErrors: string[] = [];
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,7 +13,6 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent {
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService);
-  usersFromHomeComponent = input.required<any>()
   cancelRegister = output<boolean>();
   model: any = {};
 
